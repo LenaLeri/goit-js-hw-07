60a93640f62ed6a3f7fd27e035aeb1e525eabe6d
Yelena Leri <yelenaleri@gmail.com>
2022-09-16 23:42:40 +0300

    add 1 ex

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const getGallery = document.querySelector(".gallery");
+
+const createGalleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+              <a class="gallery__link" href="${original}">
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  data-source="${original}"
+                  alt="${description}"
+                />
+              </a>
+          </div>`;
+  })
+  .join("");
+
+getGallery.insertAdjacentHTML("beforeend", createGalleryMarkup);
+
+getGallery.addEventListener("click", onImgClick);
+
+function onImgClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+		<img src = '${e.target.dataset.source}' width="800" height="600"/>
+	`);
+
+  instance.show();
+
+  document.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
